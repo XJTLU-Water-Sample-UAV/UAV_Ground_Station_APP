b1e19c7e0abfd0db700ed4244597520a8387ea37
28063 <2806353487@qq.com>
2022-02-02 22:31:53 +0800

    2.2

---
+++ app/src/main/java/com/uav_app/front_end/map_activity/managers/ButtonManager.java
@@ -0,0 +1,10 @@
+package com.uav_app.front_end.map_activity.managers;
+
+import com.uav_app.front_end.map_activity.MapActivity;
+
+public class ButtonManager extends Manager {
+    protected ButtonManager(MapActivity activity, int LISTENER_ID) {
+        super(activity, LISTENER_ID);
+    }
+
+}
