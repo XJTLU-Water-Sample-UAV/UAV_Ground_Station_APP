092389f9d8f117c527e95f97650c69d682de35e6
28063 <2806353487@qq.com>
2022-02-03 11:30:28 +0800

    2.3

--- app/src/main/java/com/uav_app/front_end/map_activity/managers/ButtonManager.java
+++ app/src/main/java/com/uav_app/front_end/map_activity/managers/ButtonManager.java
@@ -1,10 +1,15 @@
 package com.uav_app.front_end.map_activity.managers;
 
 import com.uav_app.front_end.map_activity.MapActivity;
+import com.uav_app.front_end.map_activity.MapActivityState;
 
-public class ButtonManager extends Manager {
-    protected ButtonManager(MapActivity activity, int LISTENER_ID) {
-        super(activity, LISTENER_ID);
+public class ButtonManager extends Manager implements MapActivityState.StateChangeListener {
+    public ButtonManager(MapActivity activity) {
+        super(activity, 0x03);
     }
 
+    @Override
+    public void onStateChange(MapActivityState mapActivityState) {
+
+    }
 }
