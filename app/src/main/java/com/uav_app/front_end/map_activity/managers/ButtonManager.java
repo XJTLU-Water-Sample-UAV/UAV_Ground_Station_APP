d7826fc16b28f9077f6b535cada400a17eae1934
28063 <2806353487@qq.com>
2022-02-17 21:02:54 +0800

    2.17

--- app/src/main/java/com/uav_app/front_end/map_activity/managers/ButtonManager.java
+++ app/src/main/java/com/uav_app/front_end/map_activity/managers/ButtonManager.java
@@ -1,16 +1,25 @@
 package com.uav_app.front_end.map_activity.managers;
 
+import android.widget.FrameLayout;
+
+import com.uav_app.back_end.uav_manager.R;
 import com.uav_app.front_end.map_activity.MapActivity;
 import com.uav_app.front_end.map_activity.MapActivityState;
 
 public class ButtonManager extends Manager implements MapActivityState.StateChangeListener {
+    // 母面板
+    private final FrameLayout button;
+
     public ButtonManager(MapActivity activity) {
         super(activity, 0x03);
+        button = activity.findViewById(R.id.button);
     }
 
     @Override
     public void init(Connector connector) {
         super.init(connector);
+
+
     }
 
     @Override
